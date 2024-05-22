acdc18ae7875b61759196f4b676be197663d2322
Juan Manuel Carranza <ikkicarranza@gmail.com>
2024-05-22 12:24:13 -0400

    Finish the prototipe

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,28 @@
-import { MailOutline } from '@mui/icons-material'
-import { Typography } from '@mui/material'
+import { AddOutlined, MailOutline } from '@mui/icons-material'
+import { IconButton, Typography } from '@mui/material'
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout'
+import { NoteView, NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, reprehenderit. Delectus non aspernatur quia at neque, dignissimos sapiente modi accusamus facere perspiciatis ipsa, impedit consectetur eius magnam! Quidem, a suscipit?</Typography>
+      <NothingSelectedView/>
+
+      {/* <NoteView/> */}
+      <IconButton
+      size='large'
+      sx={{
+        color: 'white',
+        backgroundColor: 'error.main',
+        ':hover': { backgroundColor: 'error.main', opacity: 0.8},
+        position: 'fixed',
+        right: 50,
+        bottom: 50
+      }}
+      >
+        <AddOutlined sx={{fontSize: 30}}/>
+      </IconButton>
     </JournalLayout>
   )
 }
