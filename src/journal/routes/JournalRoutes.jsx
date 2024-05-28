ec16cbc8ef4a157224e012fc8ae4a2b0276c7bd6
Juan Manuel Carranza <ikkicarranza@gmail.com>
2024-05-28 11:38:47 -0400

    Mantener sesion de firebase activa

--- src/journal/routes/JournalRoutes.jsx
+++ src/journal/routes/JournalRoutes.jsx
@@ -5,7 +5,7 @@ import { JournalPage } from "../pages/JournalPage"
 export const JournalRoutes = () => {
   return (
     <Routes>
-        <Route path="/*" element={ <JournalPage/> } />
+        <Route path="/" element={ <JournalPage/> } />
         <Route path="/*" element={ <Navigate to="/" /> } />
     </Routes>
   )
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
 import { JournalRoutes } from '../journal/routes/JournalRoutes'
+import { useDispatch, useSelector } from 'react-redux'
+import { CheckingAuth } from '../ui'
+
+import { useCheckAuth } from '../hooks'
 
 export const AppRouter = () => {
+const { status} = useCheckAuth();
+
+if(status==='checking') {
+  return <CheckingAuth/>
+}
+
   return (
     <Routes>
-        {/* Login y registro */}
-        <Route path='/auth/*' element={<AuthRoutes/>} />
-        {/* Journal App */}
-        <Route path='/*' element={<JournalRoutes/>}/>
+      {
+        status==='authenticated' 
+        ? <Route path='/*' element={<JournalRoutes/>}/>
+        :  <Route path='/auth/*' element={<AuthRoutes/>} />
+      }
+      <Route path='/*' element={<Navigate to='/auth/login'/>}/>
     </Routes>
   )
 }
